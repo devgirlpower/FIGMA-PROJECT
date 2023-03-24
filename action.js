bcb6e56ed8f81b2dea1fbb24a8aae44f1dbfc03b
TiagoFCorrea <tiagoflorescorrea@gmail.com>
2023-03-24 13:39:25 -0300

    botando limitações de tela e js

---
+++ action.js
@@ -0,0 +1,17 @@
+console.log("from script file");
+function sizeOfThings(){
+  var windowWidth = window.innerWidth;
+  var windowHeight = window.innerHeight;
+  
+  var screenWidth = screen.width;
+  var screenHeight = screen.height;
+  
+  document.querySelector('.window-size').innerHTML = windowWidth + 'x' + windowHeight;
+  document.querySelector('.screen-size').innerHTML = screenWidth + 'x' + screenHeight;
+
+};
+sizeOfThings();
+
+window.addEventListener('resize', function(){
+	sizeOfThings();
+});
