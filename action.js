60ed1ba1996edcc1202dfd8e3dc735ffef2fa023
TiagoFCorrea <tiagoflorescorrea@gmail.com>
2023-03-28 23:36:48 -0300

    infrator

--- action.js
+++ action.js
@@ -12,6 +12,34 @@ function sizeOfThings(){
 };
 sizeOfThings();
 
-window.addEventListener('resize', function(){
-	sizeOfThings();
-});
+
+var btn = document.querySelector("#btn");
+var infrator = document.querySelector("#infrator");
+var item = document.querySelector(".item");
+var item2 = document.querySelector(".item2");
+
+
+// INICIO, FUNÇÃO CLICK PARA ALTERAR O TAMANHO DO ITEM DE BUSCA
+//
+btn.addEventListener("click", function(){
+    if(btn.classList.contains('positivo')) {    
+      Abre();
+    } else{
+      Fecha();
+    }
+})
+
+function Abre(){
+  infrator.classList.remove('item')
+  infrator.classList.add('item2')
+  btn.classList.remove('positivo');
+}
+function Fecha(){
+  infrator.classList.remove('item2')
+  infrator.classList.add('item')
+  btn.classList.add('positivo');
+}
+//
+//  FINAL, DAS FUNÇÕES QUE COMPOEM A FUNÇÃO CLICK
+
+
